9c033b67f8c9068c2da528ed1dcafcbe0fe4c498
mukulojha500 <122665729+mukulojha500@users.noreply.github.com>
2023-07-08 17:15:05 +0530

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,82 @@
+const chat = [
+  {
+    chatId: 1,
+    sender: "Alice",
+    message: "Hi Bob, how are you?",
+    timestamp: "2023-06-28T09:12:00",
+    groupChat: false,
+  },
+  {
+    chatId: 2,
+    sender: "Bob",
+    message: "Hey Alice! I'm good, thanks for asking. How about you?",
+    timestamp: "2023-06-28T09:13:42",
+    groupChat: false,
+  },
+  {
+    chatId: 3,
+    sender: "Alice",
+    message:
+      "I'm doing great as well. Did you finish that project we were working on?",
+    timestamp: "2023-06-28T09:14:23",
+    groupChat: false,
+  },
+  {
+    chatId: 4,
+    sender: "Bob",
+    message: "Yes, I completed it yesterday. It turned out really well!",
+    timestamp: "2023-06-28T09:15:10",
+    groupChat: false,
+  },
+  {
+    chatId: 5,
+    sender: "Alice",
+    message: "That's awesome! We should celebrate our success.",
+    timestamp: "2023-06-28T09:16:27",
+    groupChat: false,
+  },
+  {
+    chatId: 6,
+    sender: "Bob",
+    message: "Absolutely! Let's plan a dinner next week.",
+    timestamp: "2023-06-28T09:17:45",
+    groupChat: false,
+  },
+  {
+    chatId: 7,
+    sender: "Alice",
+    message: "Sounds good to me. I'll check my schedule and get back to you.",
+    timestamp: "2023-06-28T09:18:55",
+    groupChat: false,
+  },
+  {
+    chatId: 8,
+    sender: "Charlie",
+    message: "Hey everyone, how's it going?",
+    timestamp: "2023-06-28T10:00:00",
+    groupChat: true,
+  },
+  {
+    chatId: 9,
+    sender: "Dave",
+    message: "I'm good, thanks!",
+    timestamp: "2023-06-28T10:01:30",
+    groupChat: true,
+  },
+  {
+    chatId: 10,
+    sender: "Eve",
+    message: "I'm doing great as well. What about you?",
+    timestamp: "2023-06-28T10:02:15",
+    groupChat: true,
+  },
+  {
+    chatId: 11,
+    sender: "Charlie",
+    message: "Glad to hear that. Let's plan a team outing next month.",
+    timestamp: "2023-06-28T10:03:45",
+    groupChat: true,
+  },
+];
+
+module.exports = { chat };
